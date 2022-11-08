d7f885d62adb7ea57d34bef1751a9e95d4e0b397
Alisson_Duarte <105180172+AlissonDuarte@users.noreply.github.com>
2022-11-08 09:42:31 -0300

    Update Requests.py
    ocultação de chaves privadas

--- FastAPI/Requests.py
+++ FastAPI/Requests.py
@@ -4,7 +4,7 @@ url = "https://graph.facebook.com/v12.0/{{111420255005189}}/message_templates?fi
 
 payload={}
 headers = {
-  'Authorization': 'Bearer {{EAAuvWQ3xYccBALbnV7mmactFX69bAMJtG19BhTvBwvZBo8kVHJZCPkGPXpGebjF3YVATeMYsZB5JJjZB6Lq7i4QLOcq6q75LXjKQ5XJRSvofyZBrnPqdjL9KRPuPfSvqnQmcmV2Qt3TiJHI87jwRXCnaAZCY0JTKk9cb6DqZBw74vPZA1LIjn5pSCprRbMSOuhUQiy6tRnXjawZDZD}}'
+  'Authorization': 'Bearer {{insira sua header aqui}}'
 }
 
 response = requests.request("GET", url, headers=headers, data=payload)
